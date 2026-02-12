faa80de228b63af6a4b363fc45c899c1ba5f1e10
toast <toast@ruka.org>
2026-02-11 17:38:56 -0800

    adjust path for multiserver yaws

--- index_app.erl
+++ index_app.erl
@@ -7,7 +7,9 @@
 out(A = #arg{server_path = "/"}) -> 
 	ws_callback:set_max_seg(A#arg.clisock, 536),
 	
-	case file:read_file("websocket.html") of
+	Filename = filename:append(filename:dirname(code:which(?MODULE)), "websocket.html"),
+
+	case file:read_file(Filename) of
 		{ok, Bin} -> {html, Bin};
 		_ -> yaws_outmod:out404(A)
 	end;
