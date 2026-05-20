f3c3a085d971373124c28e20bbbc4eeb5c56a737
toast <toast@ruka.org>
2026-05-20 10:18:33 -0700

    add thumbprint for acme challenge; add note for mss limit

--- index_app.erl
+++ index_app.erl
@@ -4,7 +4,9 @@
 -export([out/1]).
 
 
-out(A = #arg{server_path = "/"}) -> 
+out(A = #arg{server_path = "/"}) ->
+	% reduce maximum segment size, so we can send the webpage even if
+	% the client sent mss is too big
 	ws_callback:set_max_seg(A#arg.clisock, 536),
 	
 	Filename = filename:append(filename:dirname(code:which(?MODULE)), "websocket.html"),
@@ -13,4 +15,21 @@ out(A = #arg{server_path = "/"}) ->
 		{ok, Bin} -> {html, Bin};
 		_ -> yaws_outmod:out404(A)
 	end;
+out(A = #arg{server_path = "/.well-known/acme-challenge/" ++ Token}) when Token /= []->
+	case lists:all(fun is_base64url/1, Token) of
+		true ->
+			Filename= filename:append(filename:dirname(code:which(?MODULE)), "thumbprint.txt"),
+			case file:read_file(Filename) of
+				{ok, Bin} -> {content, "text/plain", [Token, ".", Bin]};
+				_ -> yaws_outmod:out404(A)
+			end;
+		false -> yaws_outmod:out404(A)
+	end;
 out(A) -> yaws_outmod:out404(A).
+
+is_base64url(C) when C >= $A, C =< $Z -> true;
+is_base64url(C) when C >= $a, C =< $z -> true;
+is_base64url(C) when C >= $0, C =< $9 -> true;
+is_base64url($-) -> true;
+is_base64url($_) -> true;
+is_base64url(_) -> false.
